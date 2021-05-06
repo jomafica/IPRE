8e09c63ec946923a962a3b2fdcc83dfe864046e1
jomafica <jomafica13.dp@gmail.com>
2021-05-06 10:42:57 +0100

    update

--- Frontend/js/bundle.js
+++ Frontend/js/bundle.js
@@ -1,3 +1,4 @@
 var form = document.querySelector('form');
 var data = new FormData(form);
 console.log(data);
+//# sourceMappingURL=bundle.js.map
--- Frontend/js/bundle.ts
+++ Frontend/js/bundle.ts
@@ -1,17 +1,4 @@
-import React from 'react';
-
 const form = document.querySelector('form')
 const data = new FormData(form);
 
 console.log(data)
-
-async function foo() {
-    try {
-        var val = await getMeAPromise();
-        console.log(val);
-    }
-    catch(err) {
-        console.log('Error: ', err.message);
-    }
-}
-
