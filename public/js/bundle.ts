63a32bfccb67b356adb7ea97de356830b0c095e0
jomafica <jomafica13.dp@gmail.com>
2021-05-06 15:27:30 +0100

    update

--- public/js/bundle.ts
+++ public/js/bundle.ts
@@ -1,13 +1,36 @@
-function searchInput() {
-    var form = (<HTMLInputElement>document.getElementById("ips")).value;
-    document.getElementById("submit").onclick = function() {
-        console.log(form);
-    }
-  };
+(function(){
 
-function reset() {
-    var form = (<HTMLInputElement>document.getElementById("ips")).value;
-    document.getElementById("reset").onclick = function() {
+    //check if area as the corret format data
+    const input = document.getElementById("ips");
+    
+    input.addEventListener("input",checktext);
+    
+    function checktext(txt){
+
+        console.log(txt.target.value);
+
+        }; 
+
+
+    //Get textarea input value
+    document.getElementById("submit").addEventListener("click",function(){
+
+        var form = (<HTMLInputElement>document.getElementById("ips")).value;
+
+        console.log(form);
+
+        } 
+    );
+
+
+    //reset table and textarea
+    document.getElementById("reset").addEventListener("click",function(){
+        
+        var form = (<HTMLInputElement>document.getElementById("ips")).value;
+
         console.log("cleaned");
-    }
-};
+
+        } 
+    );
+
+})();
