4680e07fbc38a9b5d4d9b40a2fddefdcbaedd3dc
jomafica <jomafica13.dp@gmail.com>
2021-05-06 17:25:06 +0100

    update

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,9 +1,35 @@
-function searchInput() {
-    var form = document.getElementById("form").value;
-    document.onsubmit = function () {
+(function () {
+    var input = document.getElementById("ips");
+    var submit = document.getElementById("submit");
+    submit.disabled = true;
+    input.addEventListener("blur", function (event) {
+        var element = event.target;
+        var regex = new RegExp(/(\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3})/);
+        if (regex.test(element.value)) {
+            console.log(element.value);
+            input.classList.add('bg-success');
+            setTimeout(function () {
+                input.classList.remove('bg-success');
+            }, 3000);
+            submit.disabled = false;
+        }
+        else {
+            submit.disabled = true;
+            input.classList.add('bg-danger');
+            setTimeout(function () {
+                input.classList.remove('bg-danger');
+            }, 3000);
+            console.log(element.value);
+        }
+        ;
+    });
+    submit.addEventListener("click", function () {
+        var form = submit.value;
         console.log(form);
-    };
-}
-;
-0;
+    });
+    document.getElementById("reset").addEventListener("click", function () {
+        var form = document.getElementById("ips").value;
+        console.log("cleaned");
+    });
+})();
 //# sourceMappingURL=bundle.js.map
--- public/js/bundle.ts
+++ public/js/bundle.ts
@@ -2,20 +2,47 @@
 
     //check if area as the corret format data
     const input = document.getElementById("ips");
+    const submit = (<HTMLInputElement>document.getElementById("submit"))
+    submit.disabled = true;
     
-    input.addEventListener("input",checktext);
-    
-    function checktext(txt){
+    input.addEventListener("blur", (event) => {
 
-        console.log(txt.target.value);
+        const element = event.target as HTMLInputElement;
 
-        }; 
+        let regex  = new RegExp(/(\d{1,3}[\.]\d{1,3}[\.]\d{1,3}[\.]\d{1,3})/);
 
+        if(regex.test(element.value)){ //is accepting also letters. rong state
+
+            console.log(element.value);
+
+            input.classList.add('bg-success');
+            setTimeout(function(){
+                input.classList.remove('bg-success');
+            }, 3000);
+            
+            submit.disabled = false;
+
+        }
+
+        else {  
+
+            submit.disabled = true;
+
+            input.classList.add('bg-danger'); 
+            setTimeout(function(){
+                input.classList.remove('bg-danger');
+            }, 3000);
+
+            console.log(element.value);
+
+        };
+
+    });
 
     //Get textarea input value
-    document.getElementById("submit").addEventListener("click",function(){
+    submit.addEventListener("click",function(){
 
-        var form = (<HTMLInputElement>document.getElementById("ips")).value;
+        var form = submit.value;
 
         console.log(form);
 
